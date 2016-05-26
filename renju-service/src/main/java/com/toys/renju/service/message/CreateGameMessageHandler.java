554df4a3270de52944b86d234faf482030ba59b6
jiaji.mjj <jiaji.mjj@hotmail.com>
2016-05-27 00:36:10 +0800

    主框架完善

--- renju-service/src/main/java/com/toys/renju/service/message/CreateGameMessageHandler.java
+++ renju-service/src/main/java/com/toys/renju/service/message/CreateGameMessageHandler.java
@@ -1,10 +1,12 @@
 package com.toys.renju.service.message;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.socket.WebSocketSession;
 
 /**
  * Created by lingyao on 16/5/27.
  */
+@Service("createGameMessageHandler")
 public class CreateGameMessageHandler implements IMessageHandler {
     @Override
     public void handle(WebSocketSession session, String content) {
--- renju-service/src/main/java/com/toys/renju/service/message/WatchGameMessageHandler.java
+++ renju-service/src/main/java/com/toys/renju/service/message/WatchGameMessageHandler.java
@@ -6,9 +6,9 @@ import org.springframework.web.socket.WebSocketSession;
 /**
  * Created by lingyao on 16/5/27.
  */
+@Service("watchGameMessageHandler")
 
-@Service("joinGameMessageHandler")
-public class JoinGameMessageHandler implements IMessageHandler {
+public class WatchGameMessageHandler implements IMessageHandler {
     @Override
     public void handle(WebSocketSession session, String content) {
 
