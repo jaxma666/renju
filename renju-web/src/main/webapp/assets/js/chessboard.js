d0974fc2ab524adf40a445fb637958b792be73ff
kxcf <kxcfzyk@msn.cn>
2016-06-29 00:13:14 +0800

    增加落子响应功能

--- renju-web/src/main/webapp/assets/js/chessboard.js
+++ renju-web/src/main/webapp/assets/js/chessboard.js
@@ -1,36 +1,46 @@
-var gridSize = 50;
-var validateSize = 700;
-var gapSize = gridSize / 2;
-var boardSize = validateSize + gapSize * 2;
-var pieceColor = "#000";
-
-function resetPiece(piece) {
-    piece.graphics
-        .beginFill("BlanchedAlmond")
-        .drawRect(0, 0, gridSize, gridSize)
-        .setStrokeStyle(1).beginStroke("#000")
-        .moveTo(0, gridSize / 2).lineTo(gridSize, gridSize / 2)
-        .moveTo(gridSize / 2, 0).lineTo(gridSize / 2, gridSize)
-        .endStroke();
-}
-
-function checkPiece(piece) {
-    piece.graphics
-        .beginFill(pieceColor)
-        .drawCircle(gridSize / 2, gridSize / 2, gridSize / 3);
-    piece.isChecked = true;
-}
-
-function precheckPiece(piece) {
-    var crossSize = 10;
-    piece.graphics
-        .setStrokeStyle(2).beginStroke("red")
-        .moveTo(gridSize / 2 - crossSize, gridSize / 2).lineTo(gridSize / 2 + crossSize, gridSize / 2)
-        .moveTo(gridSize / 2, gridSize / 2 - crossSize).lineTo(gridSize / 2, gridSize / 2 + crossSize)
-        .endStroke();
-}
+// 导出API：设置棋子
+// (row, col, color)
+var onSetPiece;
 
 function initChessBoard() {
+    // 常量
+    var gridSize = 50;
+    var validateSize = 700;
+    var gapSize = gridSize / 2;
+    var boardSize = validateSize + gapSize * 2;
+
+    // 本方颜色
+    var selfColor = '#000';
+
+    // 工具函数
+    function resetPiece(piece) {
+        piece.graphics
+            .beginFill("BlanchedAlmond")
+            .drawRect(0, 0, gridSize, gridSize)
+            .setStrokeStyle(1).beginStroke("#000")
+            .moveTo(0, gridSize / 2).lineTo(gridSize, gridSize / 2)
+            .moveTo(gridSize / 2, 0).lineTo(gridSize / 2, gridSize)
+            .endStroke();
+    }
+
+    // 工具函数
+    function checkPiece(piece, pieceColor) {
+        piece.graphics
+            .beginFill(pieceColor)
+            .drawCircle(gridSize / 2, gridSize / 2, gridSize / 3);
+        piece.isChecked = true;
+    }
+
+    // 工具函数
+    function precheckPiece(piece) {
+        var crossSize = 10;
+        piece.graphics
+            .setStrokeStyle(2).beginStroke("red")
+            .moveTo(gridSize / 2 - crossSize, gridSize / 2).lineTo(gridSize / 2 + crossSize, gridSize / 2)
+            .moveTo(gridSize / 2, gridSize / 2 - crossSize).lineTo(gridSize / 2, gridSize / 2 + crossSize)
+            .endStroke();
+    }
+
     var stage = new createjs.Stage("chessboard");
     stage.enableMouseOver();
 
@@ -43,16 +53,36 @@ function initChessBoard() {
     // stage.addChild(backgroud);
 
     var gridCount = validateSize / gridSize;
+
+
+    var locationMap = new Array();
+
+    function setPiece(row, col, color) {
+        checkPiece(locationMap[row][col], color);
+        stage.update();
+    }
+
+    // 导出
+    onSetPiece = setPiece;
+
     for (var row = 0; row <= gridCount; ++row) {
+
+        locationMap[row] = new Array();
+
         for (var col = 0; col <= gridCount; ++col) {
             var piece = new createjs.Shape();
+
+            locationMap[row][col] = piece;
+            piece.row = row;
+            piece.col = col;
+
             resetPiece(piece);
             piece.x = col * gridSize + gapSize - gridSize / 2;
             piece.y = row * gridSize + gapSize - gridSize / 2;
 
             piece.on("click", function (event) {
                 if (!event.target.isChecked) {
-                    checkPiece(event.target);
+                    checkPiece(event.target, selfColor);
                     stage.update();
                 }
             });
@@ -74,12 +104,8 @@ function initChessBoard() {
         }
     }
 
-
     stage.x = 0;
     stage.y = 0;
     stage.update();
 
-    function setPiece(row, col, color) {
-        // body...
-    }
 }
